efb7f600354dbc5475cc8c475128e5b7a0e1b0ec
shaik-chandu-au13 <shaikchandu24@gmail.com>
2021-10-27 11:19:20 +0530

    final

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Square from './Square'
+
+export default class board extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
